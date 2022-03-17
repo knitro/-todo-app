9f87d8bb0ec699b23fa87851333bc8c6f07157c7
Calvin Lee <calvin.hanbee.lee@gmail.com>
2022-03-17 23:39:30 +1300

    Add category and colour enums

---
+++ src/enums/colours.tsx
@@ -0,0 +1,12 @@
+export enum Colours {
+  Red     = "FF0000",
+  Orange  = "FF9C00",
+  Yellow  = "FFF600",
+  Green   = "7AFF00",
+  Cyan    = "00FFCD",
+  Blue    = "0099FF",
+  Dark_Blue = "0010FF",
+  Purple  = "7A00FF",
+  Magenta = "F000FF",
+  Black   = "000000",
+}
