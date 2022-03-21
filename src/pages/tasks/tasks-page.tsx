a7c5445f615a728754a2cbe345735a734c764b24
Calvin Lee <calvin.hanbee.lee@gmail.com>
2022-03-21 21:45:11 +1300

    Add refresher to tasks page

--- src/pages/tasks/tasks-page.tsx
+++ src/pages/tasks/tasks-page.tsx
@@ -1,8 +1,9 @@
-import { IonCardContent, IonContent, IonList, IonPage, IonText } from "@ionic/react";
+import { IonCardContent, IonContent, IonList, IonPage, IonRefresher, IonRefresherContent, IonText, RefresherEventDetail } from "@ionic/react";
 import React, { useEffect, useState } from "react";
 import { v4 } from "uuid";
 import Header from "../../components/general/Header/Header";
 import TaskItem from "../../components/task-item/task-item";
+import { auth } from "../../firebase/firebase";
 import { getTasks } from "../../firebase/firestore/firestore-tasks";
 import { Task } from "../../interfaces/tasks";
 
@@ -37,6 +38,20 @@ const TasksPage : React.FC<Props> = (props : Props) => {
     return function cleanup() {}
   }, []);
 
+  /**
+   * The refresh function for the Refresher.
+   * It gets the current position, and send it back to the Parent Component to refresh the page
+   */
+   async function doRefresh(event: CustomEvent<RefresherEventDetail>) : Promise<void> {
+    setTimeout(() => {
+      event.detail.complete();
+    }, 5000);
+    
+    const tasks = await getTasks()
+    setTasks(tasks)
+    event.detail.complete();
+  }
+
   ////////////////////////
   // Return
   ////////////////////////
@@ -45,6 +60,11 @@ const TasksPage : React.FC<Props> = (props : Props) => {
     <IonPage>
       <Header headerLabel="To Do"/>
       <IonContent>
+
+        <IonRefresher slot="fixed" onIonRefresh={doRefresh} id="refresher">
+          <IonRefresherContent></IonRefresherContent>
+        </IonRefresher>
+
         <IonList>
             {
               (tasks.length === 0)
