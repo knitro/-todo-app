5bb53d05f2d427e28a81c2af0de4b482899e62e0
Calvin Lee <calvin.hanbee.lee@gmail.com>
2022-10-29 19:41:51 +1300

    Remove excess props in chips

--- src/components/general/Chip/chip-group.tsx
+++ src/components/general/Chip/chip-group.tsx
@@ -1,76 +1,76 @@
-import React, { useState } from 'react';
-import Chip from './chip';
-import { v4 } from 'uuid';
-import { useIonAlert } from '@ionic/react';
+import React, { useState } from "react";
+import Chip from "./chip";
+import { v4 } from "uuid";
+import { useIonAlert } from "@ionic/react";
 
 ////////////////////////////////////////////////////////
 /* Props */
 ////////////////////////////////////////////////////////
 
 interface Props {
-  labels : string[]
-  setterFunction? : (a : string) => void
-  allowSelection? : boolean
-  allowCustom? : boolean
-  customString? : string
+  labels: string[];
+  setterFunction?: (a: string) => void;
+  allowSelection?: boolean;
+  allowCustom?: boolean;
+  customString?: string;
 }
 
 ////////////////////////////////////////////////////////
 /* Component */
 ////////////////////////////////////////////////////////
 
-const ChipGroup: React.FC<Props> = (props : Props) => {
-
+const ChipGroup: React.FC<Props> = (props: Props) => {
   //////////////////////////////
   /* Variable Initialisation */
   //////////////////////////////
 
   const uid = v4(); // This is the prefix for the generated chips
-  const labels : string[] = props.labels;
-  const customString : string = (props.customString) ? props.customString : "Value"
-  const setterFunction : (a : string) => void = (a : string) => {
+  const labels: string[] = props.labels;
+  const customString: string = props.customString
+    ? props.customString
+    : "Value";
+  const setterFunction: (a: string) => void = (a: string) => {
     if (props.allowSelection) {
       if (a === "+") {
         presentAlert({
-          header: 'Custom ' + customString,
-          buttons: [ 
+          header: "Custom " + customString,
+          buttons: [
             {
-              text: "Cancel"
+              text: "Cancel",
             },
             {
-              text: 'OK',
+              text: "OK",
               handler: (data) => {
-                const inputValue = data[0].trim()
+                const inputValue = data[0].trim();
                 if (inputValue !== "+" && inputValue !== "") {
-                  setCustomChipLabel(inputValue)
-                  setSelected(inputValue)
+                  setCustomChipLabel(inputValue);
+                  setSelected(inputValue);
                 }
-              }
+              },
             },
           ],
           inputs: [
             {
               placeholder: customString,
             },
-          ]
-        })
-      }
-      else if (a !== selected) {
-        setSelected(a)
+          ],
+        });
+      } else if (a !== selected) {
+        setSelected(a);
       }
     }
     if (props.setterFunction) {
-      props.setterFunction(a)
+      props.setterFunction(a);
     }
-  }
-  const allowCustom = (props.allowCustom) ? props.allowCustom : false
+  };
+  const allowCustom = props.allowCustom ? props.allowCustom : false;
 
   //////////////////////////////
   /* Hooks */
   //////////////////////////////
 
   const [selected, setSelected] = useState("");
-  const [customChipLabel, setCustomChipLabel] = useState("")
+  const [customChipLabel, setCustomChipLabel] = useState("");
   const [presentAlert] = useIonAlert();
 
   //////////////////////////////
@@ -79,25 +79,41 @@ const ChipGroup: React.FC<Props> = (props : Props) => {
 
   return (
     <div>
-      {
-        labels.map((current : string, index : number) =>
-          <Chip key={uid + "_" + index} id={uid + "_" + index} label={current} setterFunction={setterFunction} isSelected={current === selected}/>
-        )
-      }
-      {
-        (allowCustom)
-          ? <>
-              {
-                (customChipLabel !== "")
-                ? <Chip key={uid + "_custom"} id={uid + "_custom"} label={customChipLabel} setterFunction={setterFunction} isSelected={customChipLabel === selected} isCustom/>
-                : <></>
-              }
-              <Chip key={uid + "_add"} id={uid + "_add"} label={"+"} setterFunction={setterFunction} />
-            </>
-        : <></>
-      }
+      {labels.map((current: string, index: number) => (
+        <Chip
+          key={uid + "_" + index}
+          id={uid + "_" + index}
+          label={current}
+          clickFunction={setterFunction}
+          isSelected={current === selected}
+        />
+      ))}
+      {allowCustom ? (
+        <>
+          {customChipLabel !== "" ? (
+            <Chip
+              key={uid + "_custom"}
+              id={uid + "_custom"}
+              label={customChipLabel}
+              clickFunction={setterFunction}
+              isSelected={customChipLabel === selected}
+              isCustom
+            />
+          ) : (
+            <></>
+          )}
+          <Chip
+            key={uid + "_add"}
+            id={uid + "_add"}
+            label={"+"}
+            clickFunction={setterFunction}
+          />
+        </>
+      ) : (
+        <></>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default ChipGroup;
